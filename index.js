4a6246edaaa6063f5125c11752c59d43a5385827
heyapple79 <126519226+heyapple79@users.noreply.github.com>
2023-02-27 21:04:29 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+document.addEventListener ('click', function () {
+        console.log ();
+        document.getElementById('demo').innerHTML = "Herrera Damian Martin"
+});
+
+
+document.getElementById ('BOTON_COLOR').addEventListener('click' , function () {
+    document.body.style.backgroundColor = '#FF5733';
+    
+});
+
+document.getElementById ('BOTON_DEFAULT').addEventListener('click' , function () {
+    document.body.style.backgroundColor = '#B53418';
+
+});
+
+
